91c0f2dfeedc8a94d4d7b8ec5d291e9730f6c105
leblancja <leblancja@outlook.com>
2025-01-13 17:25:31 -0400

    finish timer

--- src/examples/03-flight-booker/index.svelte
+++ src/examples/03-flight-booker/index.svelte
@@ -1,49 +1,58 @@
 <script lang="ts">
+	type Option = 'one-way' | 'round-trip';
+	function getDate() {
+		const date = new Date();
+		const [month, day, year] = date
+			.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })
+			.split('/');
+		return `${year}-${month}-${day}`;
+	}
 
-    type Option = 'one-way' | 'round-trip'
-    function getDate() {
-        const date = new Date()
-        const [month, day, year] = date
-            .toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})
-            .split('/')
-        return `${year}-${month}-${day}`
-    }
+	function handleSubmit(e: Event) {
+		e.preventDefault();
+		alert(`Booked a ${selectedOption}`);
+	}
 
-    function handleSubmit(e: Event) {
-        e.preventDefault()
-        alert(`Booked a ${selectedOption}`)
-    }
-
-    let selectedOption = $state<Option>('one-way')
-    let startDate = $state(getDate())
-    let returnDate = $state(getDate())
+	let selectedOption = $state<Option>('one-way');
+	let startDate = $state(getDate());
+	let returnDate = $state(getDate());
 </script>
 
 <div class="container">
-    <h1>Flight Booker</h1>
-    <form onsubmit={handleSubmit}>
-        <select bind:value={selectedOption}>
-            <option value="one-way">one-way flight</option>
-            <option value="round-trip">two-way flight</option>
-        </select>
-       
-        <label>
-            <p> Starting Date</p>
-            <input type="date" bind:value={startDate} min={getDate()} required />
-        </label>
-        <label>
-            <p> Return Date</p>
-            <input type="date" bind:value={returnDate} min={getDate()} disabled={selectedOption !== 'round-trip'} required />
-        </label>
-    
-        <button type="submit" disabled={!startDate || (selectedOption === 'round-trip' && returnDate < startDate)}>
-            Book
-        </button>
-    </form>
+	<h1>Flight Booker</h1>
+	<form onsubmit={handleSubmit}>
+		<select bind:value={selectedOption}>
+			<option value="one-way">one-way flight</option>
+			<option value="round-trip">two-way flight</option>
+		</select>
+
+		<label>
+			<p>Starting Date</p>
+			<input type="date" bind:value={startDate} min={getDate()} required />
+		</label>
+		<label>
+			<p>Return Date</p>
+			<input
+				type="date"
+				bind:value={returnDate}
+				min={getDate()}
+				disabled={selectedOption !== 'round-trip'}
+				required
+			/>
+		</label>
+
+		<button
+			type="submit"
+			disabled={!startDate || (selectedOption === 'round-trip' && returnDate < startDate)}
+		>
+			Book
+		</button>
+	</form>
 </div>
 
 <style>
-    form p {
-        margin-bottom: 0;
-    }
+	form p {
+		margin-bottom: 0;
+	}
 </style>
+
---
+++ src/examples/04-timer/index.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	let duration: number = $state(0);
+	let elapsed: number = $state(0);
+	let interval: number;
+
+	function startTimer() {
+		interval = setInterval(() => {
+			elapsed += 0.1;
+			if (elapsed > duration) {
+				elapsed = duration;
+				clearInterval(interval);
+			}
+		}, 100);
+	}
+
+	function reset() {
+		elapsed = 0;
+		clearInterval(interval);
+		startTimer();
+	}
+
+	$effect(() => {
+		if (!duration) return;
+		startTimer();
+		return () => clearInterval(interval);
+	});
+</script>
+
+<div class="container">
+	<div>
+		<label>
+			<span>Elapsed time:</span>
+			<progress max={duration} value={elapsed}></progress>
+		</label>
+		<div>{elapsed.toFixed(1)}s</div>
+	</div>
+	<label>
+		<span>Duration:</span>
+		<input type="range" bind:value={duration} min="1" max="10" />
+	</label>
+	<button onclick={reset}>Reset</button>
+</div>
